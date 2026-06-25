63e6c46b3e8196904d600dcfbae14ce2d64df0d9
Abhijit Adhikary <35757079+abhijitadhikary@users.noreply.github.com>
2026-06-25 18:29:49 +0100

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.py
@@ -0,0 +1,17 @@
+from collections import deque
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
+        if digits[-1] < 9:
+            digits[-1] += 1
+            return digits
+        
+        carry = 1
+        list_ret = deque([0])
+        for idx in range(len(digits)-2, -1, -1):
+            sum_c = digits[idx] + carry
+            carry = sum_c // 10
+            list_ret.appendleft(sum_c % 10)
+        if carry > 0:
+            list_ret.appendleft(carry)
+        return list(list_ret)
